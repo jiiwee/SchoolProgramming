0d717f4c1b6af76ab4291c4af7d463b32d003cce
Hea7hcliff <panu.ri.partanen@hotmail.com>
2016-02-01 09:47:31 +0200

    Revert "Demo3 Fixed"
    This reverts commit 65fe330cad62721386b99a08b92bf50996cf5e0a.

--- C#/OOB/OOB/Firstone.cs
+++ C#/OOB/OOB/Firstone.cs
@@ -39,7 +39,7 @@ namespace OOB
         {
             Vehicle truck = new Vehicle("John Deere", 200, 16);
             truck.PrintData();
-            truck.name = "Car";
+            truck.name = "Samiauto";
             truck.PrintData();
         }
     }
@@ -60,14 +60,6 @@ namespace OOB
         public int age { get; set; }
         public float average { get; set; }
         public string school { get; set; }
-
-        public Student(string name, int age, float average, string school)
-        {
-            this.name = name;
-            this.age = age;
-            this.average = average;
-            this.school = school;
-        }
     }
 
     class Exercise5
@@ -76,19 +68,39 @@ namespace OOB
         {
             string[] Students = new string[5];
 
-            Student person1 = new Student("Name1", 11, 6.2f, "School1");                   
+            Student person1 = new Student();
+            person1.name = "Mikko Reipas";
+            person1.age = 21;
+            person1.average = 4;
+            person1.school = "JAMK";          
             Students[0] = person1.name + " " + person1.age + " " + person1.average + " " + person1.school;
             
-            Student person2 = new Student("Name2", 12, 7.3f, "School2");
+            Student person2 = new Student();
+            person2.name = "Sami Koodi";
+            person2.age = 1;
+            person2.average = 10;
+            person2.school = "Ex Machina";
             Students[1] = person2.name + " " + person2.age + " " + person2.average + " " + person2.school; ;
 
-            Student person3 = new Student("Name3", 13, 8.4f, "School3");
+            Student person3 = new Student();
+            person3.name = "Panu Toivo";
+            person3.age = 20;
+            person3.average = 5;
+            person3.school = "JAMK";
             Students[2] = person3.name + " " + person3.age + " " + person3.average + " " + person3.school; ;
 
-            Student person4 = new Student("Name4", 14, 9.5f, "School4");
+            Student person4 = new Student();
+            person4.name = "Kalle Jauheliha";
+            person4.age = 6;
+            person4.average = (float)7.2;
+            person4.school = "Liha";
             Students[3] = person4.name + " " + person4.age + " " + person4.average + " " + person4.school; ;
 
-            Student person5 = new Student("Name5", 15, 10.0f, "School5");
+            Student person5 = new Student();
+            person5.name = "Artur Venäläinen";
+            person5.age = 49;
+            person5.average = 7;
+            person5.school = "Raja";
             Students[4] = person5.name + " " + person5.age + " " + person5.average + " " + person5.school; ;
 
             for ( int  x = 0; x < Students.Length;x++)
@@ -110,11 +122,11 @@ namespace OOB
 {
     class Alcohol
     {
-        public string brand { get; set; }
-        public string type { get; set; }
-        public float alcprc { get; set; }
-        public string container { get; set; }
-        private int Rating{ get; set; }
+        public string brand;
+        public string type;
+        public float alcprc;
+        public string container;
+        private int Rating;
         public int rating
         {
             get { return Rating; }
@@ -126,15 +138,6 @@ namespace OOB
         }
         private readonly int MaxRating = 5;
 
-        public Alcohol(string brand, string type, float alcprc, string container, int rating)
-        {
-            this.brand = brand;
-            this.type = type;
-            this.alcprc = alcprc;
-            this.container = container;
-            this.rating = rating;
-        }
-
         public void PrintData()
         {
             Console.WriteLine("Alcohol database : ");
@@ -142,14 +145,23 @@ namespace OOB
             Console.WriteLine("- Type : " + type);
             Console.WriteLine("- Alcohol procent : " + alcprc);
             Console.WriteLine("- Rating : " + rating + "\n");
-        }       
+        }
     }
 
     class Exercise6
     {
         static void Main(string[] args)
         {
-            Alcohol alc1 = new Alcohol("brand", "type", 15.5f, "container", 5);
+            
+
+            Alcohol alc1 = new Alcohol();
+            alc1.brand = "Karjala";
+            alc1.type = "Beer";
+            alc1.alcprc = (float)4.7;
+            alc1.rating = 5;
+            alc1.container = "Can";
+           
+
             alc1.PrintData();
         }
     }
