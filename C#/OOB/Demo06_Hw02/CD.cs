8c4dde3000c377254dfaa725ed78ce8e03c90085
Hea7hcliff <panu.ri.partanen@hotmail.com>
2016-02-09 12:27:04 +0200

    Demo06_Hw02 Done

---
+++ C#/OOB/Demo06_Hw02/CD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06_Hw02
+{
+    class CD
+    {
+        public string Song { get; set; }
+        public string Artist { get; set; }
+        public float Length { get; set; }
+
+        public override string ToString()
+        {
+            return "Song: " + Song + " | Artist: " + Artist + " | Length: " + Length;
+        }
+    }
+}
---
+++ C#/OOB/Demo06_Hw02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06_Hw02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CD> songs = new List<CD>();
+
+            songs.Add(new CD { Song = "Take Your Time", Artist = "Grafix", Length = 5.51f });
+            songs.Add(new CD { Song = "Essence", Artist = "Skrux", Length = 3.38f });
+            //Käy läpi ensimmäisen (0)
+            Console.WriteLine(songs.ElementAt(0).ToString());
+
+            Console.WriteLine();
+
+            //Käy läpi jokaisen
+            foreach (CD song in songs)
+            {
+                Console.WriteLine(song.ToString());
+            }
+        }
+    }
+}
