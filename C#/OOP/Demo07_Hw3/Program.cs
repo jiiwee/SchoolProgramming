03f93ade2e19a9718f789c509fba63d5b4319ee8
Hea7hcliff <panu.ri.partanen@hotmail.com>
2016-02-16 12:38:44 +0200

    Demo07 Done

--- C#/OOP/Demo07_Hw3/Program.cs
+++ C#/OOP/Demo07_Hw3/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Demo07_Hw3
 {
@@ -10,6 +13,22 @@ namespace Demo07_Hw3
     {
         static void Main(string[] args)
         {
+            List<TVProgram> TVP = new List<TVProgram>();
+            TVP.Add(new TVProgram("Heathcliff", "MTV3", "15:00", "15:40", "Story about cats"));
+            TVP.Add(new TVProgram("Biker Mice from Mars", "MTV3", "18:00", "18:30", "Hanat auki"));
+     
+            Stream writeMultipleStream = new FileStream("MyPersons.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(writeMultipleStream, TVP);
+            writeMultipleStream.Close();
+
+            Stream readStream = new FileStream("MyPersons.bin", FileMode.Open, FileAccess.Read, FileShare.None);
+            List<TVProgram> readTVP = (List<TVProgram>) formatter.Deserialize(readStream);
+
+            foreach (TVProgram p in readTVP)
+            {
+                Console.WriteLine(p.ToString());
+            }
         }
     }
 }
---
+++ C#/OOP/Demo07_Hw3/TVProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07_Hw3
+{
+    [Serializable]
+    class TVProgram
+    {        
+        public string Name { get; set; }
+        public string Channel { get; set; }
+        public string Startingtime { get; set; }
+        public string Endingtime { get; set; }
+        public string Info { get; set; }
+
+        public TVProgram(string name, string channel, string st, string et, string info)
+        {
+            Name = name;
+            Channel = channel;
+            Startingtime = st;
+            Endingtime = et;
+            Info = info;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\n" + "Channel: " + Channel + "\n" + "Starting time: " + Startingtime
+                + "\n" + "Ending time: " + Endingtime + "\n" + "Info: " + Info + "\n";
+        }
+    }
+}
