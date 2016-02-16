9aa9b25d7345d56c0078f2f9a8c5d58291ab6e9d
Hea7hcliff <panu.ri.partanen@hotmail.com>
2016-02-16 09:55:50 +0200

    Demo07 started

---
+++ C#/OOP/Demo07_Hw2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07_Hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            double num2;
+            System.IO.StreamWriter intfile = new System.IO.StreamWriter("int.txt");
+            System.IO.StreamWriter doublefile = new System.IO.StreamWriter("double.txt");
+            while (true)
+            {
+                Console.Write("Give a number: ");
+                string value = Console.ReadLine();
+                if (int.TryParse(value, out num1))
+                {
+                    intfile.WriteLine(value);
+                }
+                else if (double.TryParse(value, out num2))
+                {
+                    doublefile.WriteLine(value);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            intfile.Close();
+            doublefile.Close();
+            string txt1 = System.IO.File.ReadAllText("int.txt");
+            Console.WriteLine("\nContents of int.txt:\n" + txt1 + "\n");
+            string txt2 = System.IO.File.ReadAllText("double.txt");
+            Console.WriteLine("Contents of double.txt:\n" + txt2);
+        }
+    }
+}
