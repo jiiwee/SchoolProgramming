f8a1715d2f8e74d5929ba883bfb80034b555e418
Hea7hcliff <panu.ri.partanen@hotmail.com>
2016-02-23 12:11:39 +0200

    Demo08_Hw06 help

---
+++ C#/OOP/Demo08_Hw06/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo08_Hw06
+{
+    class Invoice
+    {
+        public string Customer { get; set; }
+
+    }
+}
---
+++ C#/OOP/Demo08_Hw06/InvoiceItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo08_Hw06
+{
+    class InvoiceItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+        public double Total
+        {
+            get
+            {
+                return Price * Quantity;
+            }
+            set
+            {
+                Total = value;
+            }
+        }
+
+        public InvoiceItem(string name, double price, double quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Price + "e " + Quantity + " pieces " + Total + "e total";
+        }
+    }
+}
