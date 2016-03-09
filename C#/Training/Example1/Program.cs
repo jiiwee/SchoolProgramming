fcce8269da6763876c2b21e77ee9c4d7a5877686
Panu Partanen <k2857@student.jamk.fi>
2016-03-09 12:24:07 +0200

    More

---
+++ C#/Training/Example1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wallet wallet = new Wallet();
+            bool running = true;
+
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Add money");
+            Console.WriteLine("2. Take money");
+            Console.WriteLine("3. Show money");
+            Console.WriteLine("4. Throw wallet away\n");
+            
+            while (running)
+            {
+                int choice = int.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        wallet.AddMoney();
+                        break;                        
+                    case 2:
+                        wallet.TakeMoney();                        
+                        break;
+                    case 3:
+                        wallet.ShowMoney();
+                        break;
+                    case 4:
+                        running = false;
+                        break;                      
+                }
+            } 
+        }
+    }
+}
---
+++ C#/Training/Example1/Wallet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Wallet
+    {
+        private float money = 0;
+        float amount = 0;              
+
+        public void AddMoney()
+        {
+            Console.Write("How much you want to add money? : ");
+            amount = float.Parse(Console.ReadLine());
+            money += amount;
+        }
+
+        public void TakeMoney()
+        {
+            Console.Write("How much you want to take money? : ");
+            amount = float.Parse(Console.ReadLine());
+
+            if (amount < money)
+            {
+                money -= amount;
+            }
+            else 
+            {
+                Console.WriteLine("Not enough money");
+                Console.WriteLine("Try again");
+            }
+        }
+
+        public void ShowMoney()
+        {
+            Console.Write("Cash now : ");
+            Console.WriteLine(money);
+        }
+    }
+}
