749d70f39679912bcc431db6fbc4054af6961394
Panu Partanen <k2857@student.jamk.fi>
2016-03-09 12:28:02 +0200

    File fixes

--- C#/Training/AbstraktiRajapinnat/Circle.cs
+++ C#/Training/AbstraktiRajapinnat/Circle.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Wallet
+namespace AbstraktiRajapinnat
 {
-    class Program
+    class Circle : DrawingObject
     {
-        static void Main(string[] args)
+        public override void Draw()
         {
+            Console.WriteLine("Draw Circle");
         }
     }
 }
